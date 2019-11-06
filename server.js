d269b0ee0c9d85d24385daa42c9dae3d5ef45a22
Michał Swoboda <michal.swoboda@outlook.com>
2019-11-06 17:33:03 -0500

    updated files

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-``
-`
-  FriendFinder
-    - .gitignore
-    - app
-      - data
-        - friends.js
-      - public
-        - home.html
-        - survey.html
-      - routing
-        - apiRoutes.js
-        - htmlRoutes.js
-    - node_modules
-    - package.json
-    - server.js
-  `
-``
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+app.listen(PORT, function () {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
